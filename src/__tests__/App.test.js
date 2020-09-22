7c342b0245d0879a0dae17c4fe5928488fa7b2e4
Ryan Scharfer <ryan.scharfer@finanzcheck.de>
2020-09-22 14:55:13 +0200

    one test file for App

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,68 +1,141 @@
 /* eslint-disable no-unused-vars */
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
 import React from "react";
-// use react-dom so you can render a React app into a DOM node
-import ReactDOM from "react-dom";
-// using the react flavor of testing-library here to use the render event
-// getQueriesForElement and fireEvent are from the the DOM Testing library
-import {
-  render,
-  screen,
-  fireEvent,
-} from "@testing-library/react";
-// the App we want to test
-import userEvent from "@testing-library/user-event";
-import App from "../components/App/App";
+import axios from 'axios';
 
-// mocking ApexCharts and ReactApexCharts to get rid of the error that is thrown
+// mocked
 import ApexCharts from "apexcharts";
 import ReactApexChart from "react-apexcharts";
+import apiFunctions from "../../api/api";
+import apiFunctionsForCampaign from "../../api/apiFunctionsForCampaign";
 
-// mocking our external components because they do not render without throwing a error
-// in Jests browser environment
-jest.mock("react-apexcharts", () => jest.fn(() => <div>Here is a chart</div>));
+import App from "./App";
+
+jest.mock('axios')
+
+jest.mock("react-apexcharts", () => (props) => {
+  const { series } = props;
+  const data = series[0].data;
+  const name = series[0].name;
+
+  const dataDivs = data.map((piece) => (
+    <div key={piece} className="fakeChartRow">{`${name}: ${piece}`}</div>
+  ));
+  return <div className="fakeChart">{dataDivs}</div>;
+});
+
 jest.mock("apexcharts", () => ({
-  exec: jest.fn(() => new Promise((res, rej) => res("uri"))),
+  exec: () => new Promise((res, rej) => res("uri")),
 }));
 
-test("<App/> renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+let container = null;
+
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
 });
 
-test("<App/> shows the Overview header when rendered", () => {
-  // render() returns the getQueriesForElement object with a bunch of query methods
-  const { getByText } = render(<App />);
-  const h3Overview = getByText("Overview", { selector: "h3" });
-  expect(h3Overview).toBeInTheDocument();
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
 });
 
-test("a click on campaigns takes you to Campaigns view", async () => {
-  const { getByText } = render(<App />);
-  fireEvent.click(getByText("Campaigns"));
+jest.mock("./api", () => () =>
+  Promise.resolve({
+    installs: [
+      { day: "monday", value: 145 },
+      { day: "tuesday", value: 256 },
+      { day: "wednesday", value: 190 },
+      { day: "thursday", value: 204 },
+      { day: "friday", value: 120 },
+      { day: "saturday", value: 156 },
+      { day: "sunday", value: 135 },
+    ],
+    revenue: [
+      { day: "monday", value: 45.7 },
+      { day: "tuesday", value: 50.3 },
+      { day: "wednesday", value: 34.6 },
+      { day: "thursday", value: 54.8 },
+      { day: "friday", value: 67.4 },
+      { day: "saturday", value: 20.5 },
+      { day: "sunday", value: 43.8 },
+    ],
+  })
+);
 
-  expect(screen.findByText("campaign 1")).not.toBeNull();
-  expect(screen.findByText("campaign 2")).not.toBeNull();
-});
+jest.mock("./apiFunctionsForCampaign", () => () =>
+  Promise.resolve([
+    {
+      id: "08a92372-9c8a-4649-b627-2e503b839c67",
+      name: "campaign 1",
+      installs: [
+        { day: "monday", value: 45 },
+        { day: "tuesday", value: 56 },
+        { day: "wednesday", value: 22 },
+        { day: "thursday", value: 52 },
+        { day: "friday", value: 30 },
+        { day: "saturday", value: 32 },
+        { day: "sunday", value: 32 },
+      ],
+    },
+    {
+      id: "edc6012f-4983-47dc-b1ce-b09dea89f847",
+      name: "campaign 2",
+      installs: [
+        { day: "monday", value: 10 },
+        { day: "tuesday", value: 15 },
+        { day: "wednesday", value: 55 },
+        { day: "thursday", value: 78 },
+        { day: "friday", value: 45 },
+        { day: "saturday", value: 50 },
+        { day: "sunday", value: 20 },
+      ],
+    },
+    {
+      id: "79cfe6c8-f267-42eb-bc01-d93e0cc8315a",
+      name: "campaign 3",
+      installs: [
+        { day: "monday", value: 45 },
+        { day: "tuesday", value: 65 },
+        { day: "wednesday", value: 54 },
+        { day: "thursday", value: 57 },
+        { day: "friday", value: 30 },
+        { day: "saturday", value: 40 },
+        { day: "sunday", value: 51 },
+      ],
+    },
+    {
+      id: "1427e2fb-36de-402c-a471-644ad67d2e8e",
+      name: "campaign 4",
+      installs: [
+        { day: "monday", value: 10 },
+        { day: "tuesday", value: 5 },
+        { day: "wednesday", value: 13 },
+        { day: "thursday", value: 23 },
+        { day: "friday", value: 11 },
+        { day: "saturday", value: 8 },
+        { day: "sunday", value: 3 },
+      ],
+    },
+  ])
+);
 
-test("a click on create takes you to Create view", () => {
-  const { getByText } = render(<App />);
-  fireEvent.click(getByText("Create"));
-  getByText("Create Campaign");
-});
+describe("render the app, and let it do all of the effects too", () => {
+  test("<App/>", async () => {
+    console.log('mocked module', axios)
+    await act(async () => {
+      render(<App />, container);
+    });
+    expect(container.querySelector("h3").textContent).toBe("Overview");
 
-test("adding a campaign adds a campaign", () => {
-  const { getByText } = render(<App />);
-  // screen.debug()
-  // go to 'Create' tab
-  fireEvent.click(getByText("Create"));
-  // enter something in input field
-  const inputField = document.querySelector("input");
-  userEvent.type(inputField, "My new campaign");
-  // click the button
-  const createButton = getByText("Create Campaign");
-  fireEvent.click(createButton);
-  // to to 'Campaigns'
-  fireEvent.click(getByText("Campaigns"));
-  // check to see if new campaign is there
-  getByText("My new campaign");
+    expect(container.textContent).toContain("installs: 145");
+    expect(container.textContent).toContain("installs: 190");
+    expect(container.textContent).toContain("installs: 204");
+
+    expect(container.textContent).toContain("revenue: 45.7");
+    expect(container.textContent).toContain("revenue: 20.5");
+    expect(container.textContent).toContain("revenue: 43.8");
+  });
 });
